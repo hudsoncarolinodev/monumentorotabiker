501ee16a234431c132f9bc23b80e5b624450fabd
Hudson Carolino <carolinohudson@gmail.com>
2024-08-24 13:30:17 -0300

    fix: remove description

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ const addresses = [
         description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
         address: 'Estr. Da Ribeira, km 71, Bocaiúva do Sul - PR, 83450-000',
         flag:'pr',
+        instagram: 'https://www.instagram.com/serpenteando.cafe/'
     },
     {
         name: 'Panda Mechanics',
@@ -14,6 +15,7 @@ const addresses = [
         description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
         address: 'Rua Francisco Castellano, 135 - sob 3 - Jardim das Américas, Curitiba - PR, 81540-370',
         flag:'pr',
+        instagram: 'https://www.instagram.com/pandamechanics/'
     },
     {
         name: 'Mirante 12',
@@ -21,6 +23,7 @@ const addresses = [
         description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
         address: 'Estrada geral, SC-390, km 12, Lauro Müller - SC, 88880-000',
         flag:'sc',
+        instagram: 'https://www.instagram.com/mirantedo12/'
     },
     {
         name: 'Rota 370 Urubici',
@@ -28,6 +31,7 @@ const addresses = [
         description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
         address: 'SC-370 - Urubici, SC, 88650-000',
         flag:'sc',
+        instagram: 'https://www.instagram.com/rota370urubici/'
     },
     {
         name: 'Rota PR 218',
@@ -35,6 +39,7 @@ const addresses = [
         description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
         address: 'R. Benedito Salles, 2671 - Carlópolis, PR, 86420-000',
         flag:'pr',
+        instagram: 'https://www.instagram.com/rotapr218/'
     },
     {
         name: 'Rota Bike Café',
@@ -42,6 +47,7 @@ const addresses = [
         description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
         address: 'Estrada. Geral Rio Milanês - Cedro Alto, Rio dos Cedros - SC, 89121-000',
         flag:'sc',
+        instagram: 'https://www.instagram.com/rotabikecafe/'
     },
     {
         name: 'Container da Serra',
@@ -49,6 +55,7 @@ const addresses = [
         description:'SC-477 - São João da Esperança, Dr. Pedrinho - SC, 89126-000',
         address: 'Estrada. Geral Rio Milanês - Cedro Alto, Rio dos Cedros - SC, 89121-000',
         flag:'sc',
+        instagram: 'https://www.instagram.com/containerdaserra/'
     },
     {
         name: '261',
@@ -56,6 +63,7 @@ const addresses = [
         description:'SC-477 - São João da Esperança, Dr. Pedrinho - SC, 89126-000',
         address: 'R. Avelino Domingues Menk, 114 - Centro, Guapiara - SP, 18310-000',
         flag:'sp',
+        instagram: 'https://www.instagram.com/parada_261/'
     },
     {
         name: 'Posto Rota 090',
@@ -63,6 +71,7 @@ const addresses = [
         description:'SC-477 - São João da Esperança, Dr. Pedrinho - SC, 89126-000',
         address: 'PR-090, 147 - Km 147, Piraí do Sul - PR, 84240-000',
         flag:'pr',
+        instagram: 'https://www.instagram.com/rota090/'
     },
     {
         name: 'Município de Jaguarão-RS',
@@ -70,6 +79,7 @@ const addresses = [
         description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
         address: 'Av. Vinte e Sete de Janeiro, 422 - Centro, Jaguarão - RS, 96300-000',
         flag:'rs',
+        instagram: 'https://www.instagram.com/p/C4YEyZ8OhXW/?locale=en_GB%2Cen_GB'
     },
     {
         name: 'Pag Bier Cervejaria',
@@ -77,6 +87,7 @@ const addresses = [
         description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
         address: 'Rod. Júlio Garcia - Paranoá, Brasília - DF',
         flag:'df',
+        instagram: 'https://www.instagram.com/padbiercervejaria/?g=5'
     },
     {
         name: 'Pit Stop Canastra',
@@ -84,6 +95,7 @@ const addresses = [
         description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
         address: 'MJGV+4M - São Roque de Minas, MG, 37928-000',
         flag:'mg',
+        instagram:'https://www.instagram.com/pitstopcanastra/'
     },
     {
         name: 'Camping e Cabanas Portal dos Campos',
@@ -91,6 +103,7 @@ const addresses = [
         description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
         address: 'Estrada Pery Pereira Costa - Itaiacoca, Ponta Grossa - PR, 84110-000',
         flag:'pr',
+        instagram: 'https://www.instagram.com/portal.dos.campos/'
     },
     {
         name: 'Parador 158',
@@ -98,6 +111,7 @@ const addresses = [
         description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
         address: 'BR 158 Km 308, n° 13325 Centro, Itaara - RS, 97185-000',
         flag:'rs',
+        instagram:'https://www.instagram.com/parador158/'
     }
 ]
 
@@ -113,6 +127,8 @@ const main = document.querySelector("main")
             <p><strong>Monumento:</strong> ${address.monument}</p>
             <p hidden><strong>Descrição:</strong> ${address.description}</p>
             <p><strong>Endereço:</strong> ${address.address}</p>
+            <p><a target="_blank" href="${address.instagram}">Instagram <img src="./src/img/instagram.png" alt="Instagram"></a><p>
+            <p><a target="_blank" href="https://www.instagram.com/rota_biker/">Instagram oficial Rota Biker <img src="./src/img/instagram.png" alt="Instagram"></a><p>
         `
         main.appendChild(section)
       })
