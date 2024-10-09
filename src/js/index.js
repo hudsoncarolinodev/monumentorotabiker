04b24dcc18961e311bdaeab06c4250339e4efb77
Hudson Carolino <carolinohudson@gmail.com>
2024-10-09 20:07:03 -0300

    update monument

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ const addresses = [
     {
         name: 'Serpenteando Café',
         monument: 'Monumento 1',
-        description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
+        description:'Passaporte/Carimbo',
         address: 'Estr. Da Ribeira, km 71, Bocaiúva do Sul - PR, 83450-000',
         flag:'pr',
         instagram: 'https://www.instagram.com/serpenteando.cafe/',
@@ -16,7 +16,7 @@ const addresses = [
     {
         name: 'Panda Mechanics',
         monument: 'Monumento 2',
-        description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
+        description:'Passaporte/Carimbo',
         address: 'Rua Francisco Castellano, 135 - sob 3 - Jardim das Américas, Curitiba - PR, 81540-370',
         flag:'pr',
         instagram: 'https://www.instagram.com/pandamechanics/',
@@ -28,7 +28,7 @@ const addresses = [
     {
         name: 'Mirante 12',
         monument: 'Monumento 3',
-        description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
+        description:'Passaporte/Carimbo',
         address: 'Estrada geral, SC-390, km 12, Lauro Müller - SC, 88880-000',
         flag:'sc',
         instagram: 'https://www.instagram.com/mirantedo12/',
@@ -40,7 +40,7 @@ const addresses = [
     {
         name: 'Rota 370 Urubici',
         monument: 'Monumento 4',
-        description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
+        description:'',
         address: 'SC-370 - Urubici, SC, 88650-000',
         flag:'sc',
         instagram: 'https://www.instagram.com/rota370urubici/',
@@ -52,7 +52,7 @@ const addresses = [
     {
         name: 'Rota PR 218',
         monument: 'Monumento 5',
-        description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
+        description:'',
         address: 'R. Benedito Salles, 2671 - Carlópolis, PR, 86420-000',
         flag:'pr',
         instagram: 'https://www.instagram.com/rotapr218/'
@@ -60,23 +60,23 @@ const addresses = [
     {
         name: 'Rota Bike Café',
         monument: 'Monumento 6',
-        description:'BR-476, KM 71, Rastro da Serpente, Bocaiúva do Sul (Passaporte/Carimbo)',
-        address: 'Estrada. Geral Rio Milanês - Cedro Alto, Rio dos Cedros - SC, 89121-000',
+        description:'',
+        address: '',
         flag:'sc',
         instagram: 'https://www.instagram.com/rotabikecafe/'
     },
     {
         name: 'Container da Serra',
         monument: 'Monumento 7',
-        description:'SC-477 - São João da Esperança, Dr. Pedrinho - SC, 89126-000',
-        address: 'Estrada. Geral Rio Milanês - Cedro Alto, Rio dos Cedros - SC, 89121-000',
+        description:'',
+        address: '',
         flag:'sc',
         instagram: 'https://www.instagram.com/containerdaserra/'
     },
     {
         name: '261',
         monument: 'Monumento 8',
-        description:'SC-477 - São João da Esperança, Dr. Pedrinho - SC, 89126-000',
+        description:'',
         address: 'R. Avelino Domingues Menk, 114 - Centro, Guapiara - SP, 18310-000',
         flag:'sp',
         instagram: 'https://www.instagram.com/parada_261/'
@@ -84,7 +84,7 @@ const addresses = [
     {
         name: 'Posto Rota 090',
         monument: 'Monumento 9',
-        description:'SC-477 - São João da Esperança, Dr. Pedrinho - SC, 89126-000',
+        description:'',
         address: 'PR-090, 147 - Km 147, Piraí do Sul - PR, 84240-000',
         flag:'pr',
         instagram: 'https://www.instagram.com/rota090/'
@@ -92,7 +92,7 @@ const addresses = [
     {
         name: 'Município de Jaguarão-RS',
         monument: 'Monumento 10',
-        description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
+        description:'',
         address: 'Av. Vinte e Sete de Janeiro, 422 - Centro, Jaguarão - RS, 96300-000',
         flag:'rs',
         instagram: 'https://www.instagram.com/p/C4YEyZ8OhXW/?locale=en_GB%2Cen_GB'
@@ -100,7 +100,7 @@ const addresses = [
     {
         name: 'Pag Bier Cervejaria',
         monument: 'Monumento 11',
-        description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
+        description:'',
         address: 'Rod. Júlio Garcia - Paranoá, Brasília - DF',
         flag:'df',
         instagram: 'https://www.instagram.com/padbiercervejaria/?g=5'
@@ -108,7 +108,7 @@ const addresses = [
     {
         name: 'Pit Stop Canastra',
         monument: 'Monumento 12',
-        description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
+        description:'',
         address: 'MJGV+4M - São Roque de Minas, MG, 37928-000',
         flag:'mg',
         instagram:'https://www.instagram.com/pitstopcanastra/'
@@ -116,7 +116,7 @@ const addresses = [
     {
         name: 'Camping e Cabanas Portal dos Campos',
         monument: 'Monumento 13',
-        description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
+        description:'',
         address: 'Estrada Pery Pereira Costa - Itaiacoca, Ponta Grossa - PR, 84110-000',
         flag:'pr',
         instagram: 'https://www.instagram.com/portal.dos.campos/'
@@ -124,10 +124,19 @@ const addresses = [
     {
         name: 'Parador 158',
         monument: 'Monumento 14',
-        description:'As margens do Rio Jaguarão entre a prefeitura local, Terra Sul Motos',
+        description:'',
         address: 'BR 158 Km 308, n° 13325 Centro, Itaara - RS, 97185-000',
         flag:'rs',
         instagram:'https://www.instagram.com/parador158/'
+    },
+    {
+        name: 'Paranamanema - Bar & Restaurante Original Piraju ',
+        monument: 'Monumento 16',
+        description:'Passaporte/Carimbo',
+        address: 'Av. Dois - FECAPI, Piraju - SP, 18800-000',
+        flag:'sp',
+        instagram:'https://www.instagram.com/baroriginalpiraju/',
+        underconstruction: 'Em construção'
     }
 ]
 
@@ -150,8 +159,8 @@ function createTemplate(address){
                
                 <img src="./src/img/${address.flag}.png" alt="${address.address}">
             </header>
-            <p><strong>Monumento:</strong> ${address.monument}</p>
-            <p hidden><strong>Descrição:</strong> ${address.description}</p>
+            <p><strong>Monumento:</strong> ${address.monument} - <span>${address.underconstruction}<span></p>
+            <p><strong>Descrição:</strong> ${address.description}</p>
             <p><strong>Endereço:</strong> ${address.address}</p>
             <p><a class="instagram" title="${address.monument}" target="_blank" href="${address.instagram}">@${address.name} <img src="./src/img/instagram.png" alt="Instagram"></a><p>
             <p><a class="instagramRotaBiker" target="_blank" href="https://www.instagram.com/rota_biker/">Instagram oficial Rota Biker <img src="./src/img/instagram.png" alt="Instagram"></a><p>
